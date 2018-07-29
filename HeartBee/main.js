33b5fd1a2f5a845934ce229e0c1e94722646f799
Ami Zou <ami.yx.zou@gmail.com>
2018-07-28 17:40:28 -0700

    init

---
+++ HeartBee/main.js
@@ -0,0 +1,103 @@
+/*<resources>
+  <string name="app_name">heartbee</string>
+
+  <string name="agora_app_id">f5e889dc03f844c4aaa5fda833f5cf26</string>
+</resources>
+*/
+
+/*
+* Here is the notification part!
+*/
+'use strict';
+
+Notification.requestPermission( function(status){
+    console.log('Notification permission: ',status);
+  }
+)
+
+if (Notification.permission === "granted") {
+  /* do our magic */
+} else if (Notification.permission === "blocked") {
+ /* the user has previously denied push. Can't reprompt. */
+} else {
+  /* show a prompt to the user */
+}
+
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      reg.showNotification('Hello world!');
+    });
+  }
+}
+
+
+if ('Notification' in window && navigator.serviceWorker) {
+  // Display the UI to let the user toggle notifications
+}
+
+function subscribeUser() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(function(reg) {
+
+      reg.pushManager.subscribe({
+        userVisibleOnly: true
+      }).then(function(sub) {
+        console.log('Endpoint URL: ', sub.endpoint);
+      }).catch(function(e) {
+        if (Notification.permission === 'denied') {
+          console.warn('Permission for notifications was denied');
+        } else {
+          console.error('Unable to subscribe to push', e);
+        }
+      });
+    })
+  }
+}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function(reg) {
+    console.log('Service Worker Registered!', reg);
+
+    reg.pushManager.getSubscription().then(function(sub) {
+      if (sub === null) {
+        // Update UI to ask user to register for Push
+        console.log('Not subscribed to push service!');
+      } else {
+        // We have a subscription, update the database
+        console.log('Subscription object: ', sub);
+      }
+    });
+  })
+   .catch(function(err) {
+    console.log('Service Worker registration failed: ', err);
+  });
+}
+/*
+*Here is the Agora part!!
+*
+*/
+
+if(!AgoraRTC.checkSystemRequirements()) {
+  alert("browser is no support webRTC");
+}
+
+/* select Log type */
+// AgoraRTC.Logger.setLogLevel(AgoraRTC.Logger.NONE);
+// AgoraRTC.Logger.setLogLevel(AgoraRTC.Logger.ERROR);
+// AgoraRTC.Logger.setLogLevel(AgoraRTC.Logger.WARNING);
+// AgoraRTC.Logger.setLogLevel(AgoraRTC.Logger.INFO);
+// AgoraRTC.Logger.setLogLevel(AgoraRTC.Logger.DEBUG);
+
+/* simulated data to proof setLogLevel() */
+AgoraRTC.Logger.error('this is error');
+AgoraRTC.Logger.warning('this is warning');
+AgoraRTC.Logger.info('this is info');
+AgoraRTC.Logger.debug('this is debug');
+
+var client, localStream, camera, microphone;
+
+var audioSelect = document.querySelector('select#audioSource');
+var videoSelect = document.querySelector('select#videoSource');
+
+client = AgoraRTC.createClient({mode:'interop'});
---
+++ HeartBee/sw.js
@@ -0,0 +1,42 @@
+'use strict';
+
+self.addEventListener('notificationclose', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+
+  console.log('Closed notification: ' + primaryKey);
+});
+
+self.addEventListener('notificationclick', function(e) {
+  var notification = e.notification;
+  var primaryKey = notification.data.primaryKey;
+  var action = e.action;
+
+  if (action === 'close') {
+    notification.close();
+  } else {
+    clients.openWindow('http://www.example.com');
+    notification.close();
+  }
+});
+
+self.addEventListener('push', function(e) {
+  var options = {
+    body: 'This notification was generated from a push!',
+    icon: 'images/example.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2'
+    },
+    actions: [
+      {action: 'explore', title: 'Explore this new world',
+        icon: 'images/checkmark.png'},
+      {action: 'close', title: 'Close',
+        icon: 'images/xmark.png'},
+    ]
+  };
+  e.waitUntil(
+    self.registration.showNotification('Hello world!', options)
+  );
+});
